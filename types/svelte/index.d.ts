46c18ba97f820582516ef9e0b46aa618e3cc644e
Jungle <devex.soft@gmail.com>
2019-05-02 18:52:06 +0500

    fix(svelte): Fix module name

--- types/svelte/index.d.ts
+++ types/svelte/index.d.ts
@@ -11,18 +11,20 @@ declare module "*.svelte" {
   export default component;
 }
 
-export function afterUpdate(fn: () => void): void;
+declare module svelte {
+  export function afterUpdate(fn: () => void): void;
 
-export function beforeUpdate(fn: () => void): void;
+  export function beforeUpdate(fn: () => void): void;
 
-export function onDestroy(fn: () => void): void;
+  export function onDestroy(fn: () => void): void;
 
-export function onMount(fn: () => void): void;
-export function onMount(fn: () => () => void): void;
+  export function onMount(fn: () => void): void;
+  export function onMount(fn: () => () => void): void;
 
-export function setContext<K, C>(key: K, context: C): void;
-export function getContext<K, C>(key: K): C;
+  export function setContext<K, C>(key: K, context: C): void;
+  export function getContext<K, C>(key: K): C;
 
-export function tick(): Promise<void>;
+  export function tick(): Promise<void>;
 
-export function createEventDispatcher<D>(): (type: string, detail: D) => void;
+  export function createEventDispatcher<D>(): (type: string, detail: D) => void;
+}
