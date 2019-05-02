bdbd2a0f88283dbd63a5cf95421110466a77341b
Jungle <devex.soft@gmail.com>
2019-05-02 10:16:00 +0500

    feat(store): Add store.d.ts

---
+++ store.d.ts
@@ -0,0 +1,32 @@
+export interface ReadableStore<T> {
+  subscribe(fn: (value: T) => void): () => void;
+}
+
+export interface WritableStore<T> extends ReadableStore<T> {
+  set(value: T): void;
+  update(fn: (value: T) => T): void;
+}
+
+export function writable<T>(
+  value: T,
+  start?: (set: (value: T) => void) => () => void
+): WritableStore<T>;
+
+export function readable<T>(
+  value: T,
+  start?: (set: (value: T) => void) => () => void
+): ReadableStore<T>;
+
+export function derived<T>(
+  stores: ReadableStore<T>,
+  fn: (value: T, set?: (value: T) => void) => T,
+  initial_value?: T
+): ReadableStore<T>;
+
+export function derived<T>(
+  stores: ReadableStore<T>[],
+  fn: (value: T[], set?: (value: T) => void) => T,
+  initial_value?: T
+): ReadableStore<T>;
+
+export function get<T>(store: ReadableStore<T>): T;
