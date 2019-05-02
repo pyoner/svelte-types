a921fdb05420e7b6c88fbd932619a4c3c6a8c7cc
Jungle <devex.soft@gmail.com>
2019-05-02 09:12:19 +0500

    feat(svelte.d.ts): Add svelte.d.ts

---
+++ svelte.d.ts
@@ -0,0 +1,15 @@
+export function afterUpdate(fn: () => void): void;
+
+export function beforeUpdate(fn: () => void): void;
+
+export function onDestroy(fn: () => void): void;
+
+export function onMount(fn: () => void): void;
+export function onMount(fn: () => () => void): void;
+
+export function setContext<K, C>(key: K, context: C): void;
+export function getContext<K, C>(key: K): C;
+
+export function tick(): Promise<void>;
+
+export function createEventDispatcher<D>(): (type: string, detail: D) => void;
